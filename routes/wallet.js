ab2afb6585029feab22fd7afbd20c5c65ff49a3e
Mohan Selvam <mohan.selvam@qashqade.com>
2026-02-13 22:04:32 +0530

    fix

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,52 +1,100 @@
 const express = require("express");
+
 const router = express.Router();
+
 const {
+
   getWalletBalance,
+
   getWalletAccount,
+
   getAllTransfers,
+
   getAllCharges,
+
   addAmountToWallet,
+
   withdrawAmount,
+
   getAllPayouts
+
 } = require("../controllers/wallet");
+
 const { celebrate, Joi, Segments } = require("celebrate");
+
 const validateJwtToken = require("../middlewares/auth");
-const requireRole = require("../middlewares/requireRole");
+
+
 
 module.exports = (app) => {
+
   
+
   router.get("/balance", validateJwtToken, getWalletBalance);
+
+
 
   router.get("/account", validateJwtToken, getWalletAccount);
 
+
+
   router.get("/transfers", validateJwtToken, getAllTransfers);
+
+
 
   router.get("/charges", validateJwtToken, getAllCharges);
 
+
+
   router.get("/payouts", validateJwtToken, getAllPayouts);
 
-  router.post(
-    "/add-amount",
-    validateJwtToken,
-    celebrate({
-      [Segments.BODY]: Joi.object().keys({
-        amount: Joi.number().min(1).required(),
-      }),
-    }),
-    addAmountToWallet
-  );
+
 
   router.post(
-    "/withdraw",
+
+    "/add-amount",
+
     validateJwtToken,
-    requireRole(["worker", "contractor"]),
+
     celebrate({
+
       [Segments.BODY]: Joi.object().keys({
+
         amount: Joi.number().min(1).required(),
+
       }),
+
     }),
-    withdrawAmount
+
+    addAmountToWallet
+
   );
+
+
+
+  router.post(
+
+    "/withdraw",
+
+    validateJwtToken,
+
+    celebrate({
+
+      [Segments.BODY]: Joi.object().keys({
+
+        amount: Joi.number().min(1).required(),
+
+      }),
+
+    }),
+
+    withdrawAmount
+
+  );
+
+
 
   app.use("/api/v1/wallet", router);
+
 };
+
