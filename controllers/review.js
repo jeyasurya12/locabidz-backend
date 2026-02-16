61aeb3b88a704117119250162b63e656b3556884
Mohan Selvam <mohan.selvam@qashqade.com>
2026-02-16 19:57:00 +0530

    fix

--- controllers/review.js
+++ controllers/review.js
@@ -5,52 +5,92 @@ const Proposal = require("../model/proposal");
 const Notification = require("../model/notification");
 const Contract = require("../model/contract");
 
+const mongoose = require("mongoose");
+
 const createReview = async (req, res) => {
   const io = req.app.get("io");
   try {
-    const review = await Review.create({
-      user: req.body.userId,
-      review: req.body.review,
-      starCount: req.body.starCount < 0 ? 1 : req.body.starCount,
-      reviewedBy: req.user._id,
-      post: req.body.postId,
+    const session = await mongoose.startSession();
+
+    let review;
+    let voteAverage = 0;
+    let voteCount = 0;
+
+    await session.withTransaction(async () => {
+      review = await Review.create(
+        [
+          {
+            user: req.body.userId,
+            review: req.body.review,
+            starCount: req.body.starCount,
+            reviewedBy: req.user._id,
+            post: req.body.postId,
+          },
+        ],
+        { session }
+      );
+
+      const ratingAgg = await Review.aggregate([
+        { $match: { user: mongoose.Types.ObjectId(req.body.userId) } },
+        {
+          $group: {
+            _id: "$user",
+            voteAverage: { $avg: "$starCount" },
+            voteCount: { $sum: 1 },
+          },
+        },
+      ]).session(session);
+
+      voteAverage = ratingAgg?.[0]?.voteAverage || 0;
+      voteCount = ratingAgg?.[0]?.voteCount || 0;
+
+      await User.updateOne(
+        { _id: req.body.userId },
+        {
+          $addToSet: { reviews: review[0]._id },
+          voteAverage,
+          voteCount,
+        },
+        { session }
+      );
+
+      await Contract.updateOne(
+        { _id: req.body.contractId },
+        {
+          $addToSet: { reviews: review[0]._id },
+        },
+        { session }
+      );
+
+      await Notification.create(
+        [
+          {
+            notification: "notification_9",
+            user: req.body.userId,
+            values: {
+              name: req.user.firstName + " " + req.user.lastName,
+              userName: req.user.firstName + " " + req.user.lastName,
+            },
+          },
+        ],
+        { session }
+      );
     });
-    const allReviews = await Review.find({
-      user: req.body.userId,
-    });
-    const voteCount = allReviews.length;
-    const voteAverage = voteCount
-      ? allReviews.reduce((acc, cv) => acc + cv.starCount, 0) / voteCount
-      : 0;
-    await User.updateOne(
-      { _id: req.body.userId },
-      {
-        $addToSet: { reviews: review._id },
-        voteAverage,
-        voteCount,
-      }
-    );
-    await Contract.updateOne(
-      { _id: req.body.contractId },
-      {
-        $addToSet: { reviews: review._id },
-      }
-    );
-    await Notification.create({
-      notification: "notification_9",
-      user: req.body.userId,
-      values: {
-        name: req.user.firstName + " " + req.user.lastName,
-        userName: req.user.firstName + " " + req.user.lastName,
-      },
-    });
+
+    session.endSession();
+
     io.to(`${req.body.userId}`).emit("RECEIVE_MESSAGE", {
       type: "NEW_NOTIFICTION",
-      data: review,
+      data: review?.[0] || null,
     });
+
     return res.sendResponse({
       message: "Your review has been submitted succesfully",
-      data: review,
+      data: {
+        review: review?.[0] || null,
+        voteAverage,
+        voteCount,
+      },
     });
   } catch (err) {
     return res.sendError({ message: err.message });
